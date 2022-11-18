2811912005d3540eaaf029221e4262c8a3d30956
csmcafee <csmcafee@wpi.edu>
2022-11-18 12:50:00 -0500

    updated emp to include radiation and conduction with the air o the outside

--- HPRC_Nozzle_Calculator.m
+++ HPRC_Nozzle_Calculator.m
@@ -23,11 +23,13 @@ beta = 0.075; % Mass fraction of solid particlesin exhaust
 %% Nozzle Temperature Inputs
 
 thermal = true; % Only turn on if the nozzle is no being cooled
-T_a = 275; % Ambient temperatre (K)
+T_a = 40; % Temperature of ambient air (F)
 K_n = 15; % Thermal conductivity of nozzle material (Wm^-1K^-1)
 C_n = 500; % Specific heat of nozzle material (m^2s^-2K^-1 | Jkg^-1K^-1)
 rho_n = 7500;  % Density of nozzle material (kgm^-3)
 t_n = 0.1; % Thickness of nozzle (in);
+emissivity = 0.5; % Emissivity of nozzle material
+h_a = 10; % Heat transfer coeffient of ambient air (Wm^-2K^-1)
 
 %% Conversions
 
@@ -41,6 +43,7 @@ a = a.*0.0254.*((6894.76).^-n); % Burn Coeffient of pressure ranges (ins^-1psi^1
 theta_c = theta_c*(pi/180); % Nozzle Converge Angles (radians)
 theta_d = theta_d*(pi/180); % Nozzle Diverge Angles (radians)
 t_n = t_n*0.0254; % Thickness of nozzle (m);
+T_a = (5/9)*(T_a-32) + 273.15; % Temperature of ambient air (K)
 
 %% Constants
 
@@ -48,6 +51,7 @@ n_a = 2.7E25; % Number Density of Air (m^-3)
 R_bar = 8.3145; % Universal Gas Constant (kgm^-1s^-2K^-1mol^-1 | Jmol^-1K^-1)
 N_A = 6.02E23; % Avagadro's Number (mol^-1)
 k_b = 1.38E-23; % Boltzman Constant (JK^-1)
+sigma = 5.67*10^-8; % Stefan-Boltzmann constant (Wm^-2K^-1)
 P_a = 101325; % Atmospheric Pressure (Pa)
 g = 9.81; % Acceleration due to Gravity (ms^-2)
 M_a = 0.02897; % Molar Mass of Air (kgmol^-1)
@@ -229,10 +233,10 @@ if(thermal)
     i = 1;
     while(t(i)<burn_time)
 
-        k1=temperature_dynamics(xCurr,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n)*dt;
-        k2=temperature_dynamics(xCurr+1/2*k1,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n)*dt;
-        k3=temperature_dynamics(xCurr+1/2*k2,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n)*dt;
-        k4=temperature_dynamics(xCurr+k3,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n)*dt;
+        k1=temperature_dynamics(xCurr,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dt;
+        k2=temperature_dynamics(xCurr+1/2*k1,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dt;
+        k3=temperature_dynamics(xCurr+1/2*k2,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dt;
+        k4=temperature_dynamics(xCurr+k3,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dt;
         xCurr=xCurr+1/6*k1+1/3*k2+1/3*k3+1/6*k4;
 
         TRec(i) = xCurr;
@@ -246,7 +250,7 @@ if(thermal)
     xlabel('Time (s)', 'FontSize', 11)
     ylabel('Temperature (K)', 'FontSize', 11)
 
-    fprintf("Max Temperature of Nozzle: %4.0fN\n\n",TRec(i-1));
+    fprintf("Max Temperature of Nozzle: %4.0fK\n\n",max(TRec));
 end
 
 %% Functions
@@ -322,7 +326,7 @@ function [k,R] = two_phase_flow(R,k,beta,c_s)
     R = (1-beta)*R;
 end
 
-function TDot = temperature_dynamics(TCurr,R,k,K_n,T0,P0,M_p,k_b,N_A,d,C_n,rho_n,t_n)
+function TDot = temperature_dynamics(TCurr,R,k,K_n,T0,P0,M_p,k_b,N_A,d,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)
     Cp = R/(k-1);
     Pr = (2/5)*k;
     mu = Pr*(K_n/Cp);
@@ -335,7 +339,7 @@ function TDot = temperature_dynamics(TCurr,R,k,K_n,T0,P0,M_p,k_b,N_A,d,C_n,rho_n
 
     St = 0.023*Re^(-1/5)*Pr^(-0.067);
     h = St*rho*v*Cp;
-    QDot = h*(T-TCurr);
+    QDot = h*(T-TCurr)-((emissivity*sigma*TCurr^4) + (h_a*(TCurr - T_a)));
     TDot = QDot/(C_n*rho_n*t_n);
 end
 
@@ -380,5 +384,4 @@ end
 %   Exhaust is choked in the throat
 %   The entire thickness of the throat is the same temperature
 %   The chamber is always at the combustion temperture
-%   No heat is lost to the air on the outside of the nozzle
 

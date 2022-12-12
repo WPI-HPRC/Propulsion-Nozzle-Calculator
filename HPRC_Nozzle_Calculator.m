6354eee271645eacb66f431c1bc748f088eecc57
csmcafee <csmcafee@wpi.edu>
2022-12-12 12:41:35 -0500

    moved throat temp to its own script

--- HPRC_Nozzle_Calculator.m
+++ HPRC_Nozzle_Calculator.m
@@ -28,15 +28,6 @@ k_p = 1.21; % Ratio of Specific Heats of Propellant
 c_s = 897; % Specific heat of solid particles in exhaust (m^2s^-2K^-1 | Jkg^-1K^-1)
 beta = 0.075; % Mass fraction of solid particlesin exhaust
 
-%   Throat Temperature 
-T_a = 40; % Temperature of ambient air (F)
-K_n = 15; % Thermal conductivity of nozzle material (Wm^-1K^-1)
-C_n = 500; % Specific heat of nozzle material (m^2s^-2K^-1 | Jkg^-1K^-1)
-rho_n = 7500;  % Density of nozzle material (kgm^-3)
-t_n = 0.1; % Thickness of nozzle (in);
-emissivity = 0.5; % Emissivity of nozzle material
-h_a = 10; % Heat transfer coeffient of ambient air (Wm^-2K^-1)
-
 %% Conversions
 
 L = L*0.0254; % Length of Casing (m)
@@ -48,8 +39,6 @@ PRangeMax = PRangeMax.*6894.76; % Max pressure of pressure ranges (psi)
 a = a.*0.0254.*((6894.76).^-n); % Burn Coeffient of pressure ranges (ins^-1psi^1n)
 theta_c = theta_c*(pi/180); % Nozzle Converge Angles (radians)
 theta_d = theta_d*(pi/180); % Nozzle Diverge Angles (radians)
-t_n = t_n*0.0254; % Thickness of nozzle (m);
-T_a = (5/9)*(T_a-32) + 273.15; % Temperature of ambient air (K)
 
 %% Constants
 
@@ -73,7 +62,6 @@ m_p = rho_p*(L_g*pi*((d_case/2)^2-(d_core/2)^2)); % Mass of Propellent (kg)
 %% Settings
 
 auto_expansion_ratio = true; % Sets if the calculator automatically calculates expansion ratio for average chamber pressure
-thermal = false; % Sets whether calculator calculates temperature of the throat over the burn
 tMaxSteps = 100000; % Maximum Amount of Steps for Chamber Pressure Calculation
 h = 0.000065; % Chamber Pressure dt Height Parameter
 s = 0.0021; % Chamber Pressure dt Shape Parameter
@@ -232,38 +220,6 @@ fprintf("Total Impulse: %5.2fNs\n",I_t);
 Isp = I_t/(m_p*g);
 fprintf("Specific Impulse: %5.2fs\n\n",Isp);
 
-%% Throat Temperature
-
-if(thermal)
-
-    xCurr = T_a;
-
-    TRec = zeros(length(t),1);
-    T(1) = xCurr;
-
-    i = 1;
-    while(t(i)<burn_time)
-
-        k1=temperature_dynamics(xCurr,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
-        k2=temperature_dynamics(xCurr+1/2*k1,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
-        k3=temperature_dynamics(xCurr+1/2*k2,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
-        k4=temperature_dynamics(xCurr+k3,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
-        xCurr=xCurr+1/6*k1+1/3*k2+1/3*k3+1/6*k4;
-
-        TRec(i) = xCurr;
-
-        i = i+1;
-    end
-    
-    figure()
-    plot(t(1:i-1),TRec(1:i-1))
-    title("Temperature of Nozzle over Burn")
-    xlabel('Time (s)', 'FontSize', 11)
-    ylabel('Temperature (K)', 'FontSize', 11)
-
-    fprintf("Max Temperature of Nozzle: %4.0fK\n\n",max(TRec));
-end
-
 %% Functions
 
 function xDot = chamber_pressure_dynamics(x,L,d_case,M_p,T_0,a,n,k_p,inhib,R,A_star,N_A,k_b,n_p,k_a,M_a)
@@ -337,19 +293,3 @@ function [k,R] = two_phase_flow(R,k,beta,c_s)
     R = (1-beta)*R;
 end
 
-function TDot = temperature_dynamics(TCurr,R,k,K_n,T0,P0,M_p,k_b,N_A,d,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)
-    Cp = R/(k-1);
-    Pr = (2/5)*k;
-    mu = Pr*(K_n/Cp);
-
-    T = T0*(1+((k-1)/2))^-1;
-    v = (k*R*T)^(1/2);
-    rho0 = (P0*M_p)/(k_b*T0*N_A);
-    rho = rho0*(1+((k-1)/2))^((k-1)/2);
-    Re = (rho*v*d)/mu;
-
-    St = 0.023*Re^(-1/5)*Pr^(-0.067);
-    h = St*rho*v*Cp;
-    QDot = h*(T-TCurr)-((emissivity*sigma*TCurr^4) + (h_a*(TCurr - T_a)));
-    TDot = QDot/(C_n*rho_n*t_n);
-end
---
+++ Throat_Temp.m
@@ -0,0 +1,62 @@
+
+%% Run this after running the main script, it does not work on its own
+
+
+T_a = 40; % Temperature of ambient air (F)
+K_n = 15; % Thermal conductivity of nozzle material (Wm^-1K^-1)
+C_n = 500; % Specific heat of nozzle material (m^2s^-2K^-1 | Jkg^-1K^-1)
+rho_n = 7500;  % Density of nozzle material (kgm^-3)
+t_n = 0.1; % Thickness of nozzle (in);
+emissivity = 0.5; % Emissivity of nozzle material
+h_a = 10; % Heat transfer coeffient of ambient air (Wm^-2K^-1)
+
+
+T_a = (5/9)*(T_a-32) + 273.15; % Temperature of ambient air (K)
+t_n = t_n*0.0254; % Thickness of nozzle (m);
+
+
+xCurr = T_a;
+
+TRec = zeros(length(t),1);
+T(1) = xCurr;
+
+i = 1;
+while(t(i)<burn_time)
+
+    k1=temperature_dynamics(xCurr,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
+    k2=temperature_dynamics(xCurr+1/2*k1,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
+    k3=temperature_dynamics(xCurr+1/2*k2,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
+    k4=temperature_dynamics(xCurr+k3,R,kRec(i),K_n,T_0,PRec(i),M_p,k_b,N_A,d_star,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)*dtRec(i);
+    xCurr=xCurr+1/6*k1+1/3*k2+1/3*k3+1/6*k4;
+
+    TRec(i) = xCurr;
+
+    i = i+1;
+end
+
+figure()
+plot(t(1:i-1),TRec(1:i-1))
+title("Temperature of Nozzle over Burn")
+xlabel('Time (s)', 'FontSize', 11)
+ylabel('Temperature (K)', 'FontSize', 11)
+
+fprintf("Max Temperature of Nozzle: %4.0fK\n\n",max(TRec));
+
+
+function TDot = temperature_dynamics(TCurr,R,k,K_n,T0,P0,M_p,k_b,N_A,d,C_n,rho_n,t_n,emissivity,sigma,h_a,T_a)
+    Cp = R/(k-1);
+    Pr = (2/5)*k;
+    mu = Pr*(K_n/Cp);
+
+    T = T0*(1+((k-1)/2))^-1;
+    v = (k*R*T)^(1/2);
+    rho0 = (P0*M_p)/(k_b*T0*N_A);
+    rho = rho0*(1+((k-1)/2))^((k-1)/2);
+    Re = (rho*v*d)/mu;
+
+    St = 0.023*Re^(-1/5)*Pr^(-0.067);
+    h = St*rho*v*Cp;
+    QDot = h*(T-TCurr)-((emissivity*sigma*TCurr^4) + (h_a*(TCurr - T_a)));
+    TDot = QDot/(C_n*rho_n*t_n);
+end
+
